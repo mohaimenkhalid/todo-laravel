e5d4788795a874864d0110d41ea9be6445fac8e0
mohaimenkhalid <mohaimen707@gmail.com>
2019-11-05 01:53:43 +0600

    Laravel Passport install and setup

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,15 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // });
 
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('details', 'AuthController@details');
+    Route::post('/logout', 'AuthController@logout')->name('logout');
+});
+
+Route::post('/login', 'AuthController@login')->name('login');
+Route::post('/register', 'AuthController@register')->name('register');
+
+
 Route::get('/todos', 'TodoController@index');
 Route::post('/todos', 'TodoController@store');
 Route::patch('/todos/{todo}', 'TodoController@update');
