d9118ac84562e91ee4cc91fa3aec601e5e92178e
mohaimenkhalid <mohaimen707@gmail.com>
2019-11-05 19:00:06 +0600

    laravel passport completely finished

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,9 +8,6 @@ use App\User;
 use Auth; 
 use Validator;
 
-
-
-
 class AuthController extends Controller
 {
     public $successStatus = 200;
@@ -28,7 +25,6 @@ class AuthController extends Controller
     }
 
 
-
     public function register(Request $request) 
     { 
         $validator = Validator::make($request->all(), [ 
@@ -38,16 +34,16 @@ class AuthController extends Controller
             'confirm_password' => 'required|same:password', 
         ]);
             if ($validator->fails()) { 
-                        return response()->json(['error'=>$validator->errors()], 401);            
-                    }
+                    return response()->json(['error'=>$validator->errors()], 401);            
+                }
 
             $input = $request->all(); 
-                    $input['password'] = bcrypt($input['password']); 
-
-                    $user = User::create($input); 
-                    $success['token'] =  $user->createToken('MyApp')-> accessToken; 
-                    $success['name'] =  $user->name;
-            return response()->json(['success'=>$success], $this-> successStatus); 
+                $input['password'] = bcrypt($input['password']); 
+                $user = User::create($input); 
+                $success['token'] =  $user->createToken('MyApp')-> accessToken; 
+                $success['name'] =  $user->name;
+            return response()->json(['success'=>$success], $this-> successStatus);
+            //return  $this->login($request);  if direct login
         }
 
           
@@ -62,7 +58,6 @@ class AuthController extends Controller
             auth()->user()->tokens->each(function ($token, $key){
                 $token->delete();
             });
-
             return response()->json('Logged out successfully done', 200); 
         }
     }
